9d8a0a6c64c9ede0bf8d252e4d1cf29081170fca
HR0101 <hararyuto0101@gmail.com>
2025-06-13 15:15:33 +0900

    Initial Commit

---
+++ BusTimeApp/BusTimeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusTimeAppApp.swift
+//  BusTimeApp
+//
+//  Created by hara ryuto   on 2025/06/13.
+//
+
+import SwiftUI
+
+@main
+struct BusTimeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BusTimeAppTests/BusTimeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BusTimeAppTests.swift
+//  BusTimeAppTests
+//
+//  Created by hara ryuto   on 2025/06/13.
+//
+
+import Testing
+@testable import BusTimeApp
+
+struct BusTimeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
