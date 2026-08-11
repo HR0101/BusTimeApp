632740f40800be5761d1e98d41af0a91b6699153
HR0101 <hararyuto0101@gmail.com>
2026-08-11 18:32:49 +0900

    Fix/live activity countdown (#10)
    * fix: Live Activityの残り時間表示を明確化
    
    * fix: Live Activityの秒表示を削除
    
    * fix: Live Activityと通常通知を併用
    
    * chore: 不要なRuby作業スクリプトを削除

--- fix.rb
+++
@@ -1,8 +0,0 @@
-require 'xcodeproj'
-project_path = 'BusTimeApp.xcodeproj'
-project = Xcodeproj::Project.open(project_path)
-widget_target = project.targets.find { |t| t.name == 'BusTimeWidgetExtension' }
-widget_target.build_configuration_list.build_configurations.each do |config|
-  config.build_settings['PRODUCT_NAME'] = '$(TARGET_NAME)'
-end
-project.save
